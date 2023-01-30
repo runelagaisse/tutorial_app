869171298966429f331781fc5b7e44ad3affd8ae
Rune Lagaisse <rune.lagaisse@vliz.be>
2023-01-30 11:20:33 +0100

    use demo app

--- tutorial_app/api.py
+++ tutorial_app/api.py
@@ -23,13 +23,23 @@ module [2].
 [2]: https://github.com/deephdc/demo_app
 """
 
-from pathlib import Path
+import pathlib
 import pkg_resources
 
 from tutorial_app.misc import _catch_error
 
+import base64
+import json
+import math
+from random import random
+import time
 
-BASE_DIR = Path(__file__).resolve().parents[1]
+from tensorboardX import SummaryWriter
+from webargs import fields, validate
+from tutorial_app.misc import _catch_error, launch_tensorboard
+
+#BASE_DIR = Path(__file__).resolve().parents[1]
+BASE_DIR = pathlib.Path("C:/Users/rune.lagaisse/tutorial_app")
 
 
 @_catch_error
@@ -62,6 +72,137 @@ def get_metadata():
 
     return meta
 
+def train(**kwargs):
+    """
+    Dummy training. We just sleep for some number of epochs (1 epoch = 1 second)
+    mimicking some computation taking place.
+    We can log some random losses in Tensorboard to mimic monitoring.
+    """
+    logdir = BASE_DIR / "runs" / time.strftime("%Y-%m-%d_%H-%M-%S")
+    writer = SummaryWriter(logdir=logdir)
+    launch_tensorboard(logdir=logdir)
+    for epoch in range(kwargs["epoch_num"]):
+        time.sleep(1.)
+        writer.add_scalar("scalars/loss", - math.log(epoch + 1), epoch)
+    writer.close()
+
+    return {"status": "done", "final accuracy": 0.9}
+
+
+def get_predict_args():
+    """
+    TODO: add more dtypes
+    * int with choices
+    * composed: list of strs, list of int
+    """
+    # WARNING: missing!=None has to go with required=False
+    # fmt: off
+    arg_dict = {
+        "demo-str": fields.Str(
+            required=False,
+            missing="some-string",
+        ),
+        "demo-str-choice": fields.Str(
+            required=False,
+            missing="choice2",
+            enum=["choice1", "choice2"],
+        ),
+        "demo-int": fields.Int(
+            required=False,
+            missing=1,
+        ),
+        "demo-int-range": fields.Int(
+            required=False,
+            missing=50,
+            validate=[validate.Range(min=1, max=100)],
+        ),
+        "demo-float": fields.Float(
+            required=False,
+            missing=0.1,
+        ),
+        "demo-bool": fields.Bool(
+            required=False,
+            missing=True,
+        ),
+        "demo-dict": fields.Str(  # dicts have to be processed as strings
+            required=False,
+            missing='{"a": 0, "b": 1}',  # use double quotes inside dict
+        ),
+        "demo-list-of-floats": fields.List(
+            fields.Float(),
+            required=False,
+            missing=[0.1, 0.2, 0.3],
+        ),
+        "demo-image": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="image",  # description needed to be parsed by UI
+        ),
+        "demo-audio": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="audio",  # description needed to be parsed by UI
+        ),
+        "demo-video": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="video",  # description needed to be parsed by UI
+        ),
+    }
+    # fmt: on
+    return arg_dict
+
+
+@_catch_error
+def predict(**kwargs):
+    """
+    Return same inputs as provided. We also add additional fields
+    to test the functionality of the Gradio-based UI [1].
+       [1]: https://github.com/deephdc/deepaas_ui
+    """
+    # Dict are fed as str so have to be converted back
+    kwargs["demo-dict"] = json.loads(kwargs["demo-dict"])
+
+    # Add labels and random probalities to output as mock
+    prob = [random() for _ in range(5)]
+    kwargs["probabilities"] = [i / sum(prob) for i in prob]
+    kwargs["labels"] = ["class2", "class3", "class0", "class1", "class4"]
+
+    # Read media files and return them back in base64
+    for k in ["demo-image", "demo-audio", "demo-video"]:
+        with open(kwargs[k].filename, "rb") as f:
+            media = f.read()
+        media = base64.b64encode(media)  # bytes
+        kwargs[k] = media.decode("utf-8")  # string (in utf-8)
+
+    return kwargs
+
+
+# Schema to validate the `predict()` output
+schema = {
+    "demo-str": fields.Str(),
+    "demo-str-choice": fields.Str(),
+    "demo-int": fields.Int(),
+    "demo-int-range": fields.Int(),
+    "demo-float": fields.Float(),
+    "demo-bool": fields.Bool(),
+    "demo-dict": fields.Dict(),
+    "demo-list-of-floats": fields.List(fields.Float()),
+    "demo-image": fields.Str(
+        description="image"  # description needed to be parsed by UI
+    ),
+    "demo-audio": fields.Str(
+        description="audio"  # description needed to be parsed by UI
+    ),
+    "demo-video": fields.Str(
+        description="video"  # description needed to be parsed by UI
+    ),
+    "labels": fields.List(fields.Str()),
+    "probabilities": fields.List(fields.Float()),
+}
 
 # def warm():
 #     pass
